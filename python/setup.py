bd72ce4e116d38418fc19b92c8f940b637fa8083
xuxu <xuxu_1988@126.com>
2014-09-28 15:20:21 +0800

    20140928

--- python/setup.py
+++ python/setup.py
@@ -6,8 +6,8 @@ setup(
     name='AdbForTest',
     version='0.2',
     packages=['adbUtils', 'adbUtils.utils'],
-    url='https://git.oschina.net/xuxu1988/Adb-For-Test.git',
-    license='',
+    url='https://github.com/gb112211/Adb-For-Test',
+    license='1.0',
     author='xumin',
     author_email='274925460@qq.com',
     description=''
